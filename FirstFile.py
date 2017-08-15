63843125f62bc9255c0d395e3e1703e179238740
maximilianhartl <Signum25>
2017-08-15 15:52:37 +0200

    5 comment PyCharm

--- FirstFile.py
+++ FirstFile.py
@@ -5,3 +5,4 @@ print('I am a second print statement in PyCharm')
 print('I am a thrid print statement in PyCharm')
 
 print('I am a forth print statement in PyCharm')
+print('I am a 5. print statement in PyCharm')
